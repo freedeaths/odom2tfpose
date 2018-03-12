8bc06a049b4ca1f54f1f27d5e9984d66c0d98581
Laura <laura_estelle_lindzey@yahoo.com>
2018-03-12 20:17:04 +0800

    adding transformation to map frame, and adding odom2tfpose to launch file

--- src/odom2tfpose/odometry2tfpose.py
+++ src/odom2tfpose/odometry2tfpose.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2.7
 
 import rospy
+import tf
 
 import geometry_msgs.msg
 import nav_msgs.msg
@@ -13,13 +14,16 @@ class OdometryTransformer(object):
         self.sub = rospy.Subscriber('/gps/rtkfix', nav_msgs.msg.Odometry, callback=self.odometry_cb)
 
         # Publishes vehicle position in the map frame
-        # QUESTION: Does this have to be in the map frame, or can we give it in GPS frame
-        # and assume that the subsriber properly transforms it?
+        # QUESTION: Does this have to be in the map frame, or can we give it in GPS frame?
+        #           I looked at ndt_estimation, and didn't see it even checking the
+        #           frame in the header...
         self.pose_pub = rospy.Publisher('/gnss_pose', geometry_msgs.msg.PoseStamped, queue_size=1)
         # Publishes whether fix is valid
-        # QUESTION: Is this required? Or do they only check for False?
-        # QUESTION: Does the autoware 
-        self.stat_pub = rospy.Publisher('/gnss_stat', std_msgs.msg.Bool)
+        # QUESTION: Is this required? The autoware config files seem to expect it,
+        #           but I couldn't find it anywhere in the code.
+        self.stat_pub = rospy.Publisher('/gnss_stat', std_msgs.msg.Bool, queue_size=1)
+
+        self.tf = tf.Transformer(True, rospy.Duration(5.0))
 
     def odometry_cb(self, msg):
 
@@ -27,9 +31,12 @@ class OdometryTransformer(object):
 
         pose_msg.header = msg.header
         pose_msg.pose.position = msg.pose.pose.position
-        
-        self.pose_pub.publish(pose_msg)
-        self.stat_pub.publish(std_msgs.msg.Bool(True))
+
+        target_frame = "map"
+        if self.tf.canTransform(target_frame, pose_msg.header.frame_id, pose_msg.header.stamp):
+            transformed_msg = self.tf.transformPose(target_frame, pose_msg)
+            self.pose_pub.publish(transformed_msg)
+            self.stat_pub.publish(std_msgs.msg.Bool(True))
 
 if __name__=="__main__":
     transformer = OdometryTransformer()
